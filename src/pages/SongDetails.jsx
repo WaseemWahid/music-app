3a9e31c5a57a8aec9e10ed1534329824c8a81ef5
Waseem <wahid.waseem@gmail.com>
2022-10-19 20:33:13 -0700

    imports and details header

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSons } from "../components";
 
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
 
 const SongDetails = () => {
-    const { songid } = useParams();
+    const dispatch = useDispatch();
+    const { songid } = useParams()
+    const { activeSong, isPlaying } = useSelector((state) => state.player)
 
     console.log(songid)
-    return <div>SongDetails</div>;
+    return (
+        <div className="flex flex-col">
+            <DetailsHeader artistId={artistId} songData={songData} />
+        </div>
+    )
 
 
 
