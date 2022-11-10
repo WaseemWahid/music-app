8474bef87d642d4cfddc8517ec8eb34848755abc
Waseem <wahid.waseem@gmail.com>
2022-11-09 22:14:46 -0800

    uncommented

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -17,7 +17,7 @@ const SongDetails = () => {
 
     return (
         <div className="flex flex-col">
-            {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+            <DetailsHeader artistId={artistId} songData={songData} />
             <div className="mb-10">
                 <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
                 <div className="empty-5">
